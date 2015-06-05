8d5ead3be6e9f5fc60437f6083cef6d47bd991ec
Joseph <Joseph@Herbert>
2015-06-05 17:01:59 -0600

    5-14-2015 Minor stuff

---
+++ Group7SolarTrails/src/byui/cit260/solarTrails/control/SynthesizerControl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.solarTrails.control;
+import byui.cit260.solarTrails.model.*;
+
+/**
+ *
+ * @author Joseph
+ */
+public class SynthesizerControl {
+    int senthesizeItem(int reqMats, int itemType, int quantity) {
+        if (reqMats < 0 || reqMats > 1000) {
+            return -1;
+        }
+        if (itemType < 1 || itemType > 30) {
+            return -1;
+        }
+        if (quantity < 1 || quantity > 100) {
+            return -1;
+        }
+        
+        for (int i = 1; i <= quantity && adequateMaterialsRemaining(reqMats, itemType) == true; i++) {
+            Inventory.minerals -= reqMats * (1 + ItemSynthesizer.getStateOfRepair());
+            Add to inventory
+            ItemSynthesizer.setStateOfRepair();
+        }
+    }
+}
