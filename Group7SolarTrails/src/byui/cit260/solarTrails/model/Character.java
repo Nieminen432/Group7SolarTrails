45202aadb4b6a204d7d785878b95dff5472e091e
Adam <hiatta@byui.edu>
2015-06-27 08:24:36 -0600

    modified character class for enum

--- Group7SolarTrails/src/byui/cit260/solarTrails/model/Character.java
+++ Group7SolarTrails/src/byui/cit260/solarTrails/model/Character.java
@@ -13,6 +13,57 @@ import java.util.Objects;
  *
  * @author Joseph
  */
+public enum Character implements Serializable {
+    
+    /*
+    Crew members are not static when game starts. Outside of this object, we will make an arraylist for our characters. 
+    However, we have modified this as directions have stated for learning purposes.
+    */
+    NPC1("Your first crew member."),
+    NPC2("Second crew member."),
+    NPC3("Third crew member."),
+    NPC4("Fourth crew member.");
+
+    
+    private final String description;
+    private final int[] coordArray = new int[2];
+    private final double health;
+
+    Character(String description) {
+        this.description = description;
+        health = 100; 
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int[] getCoordArray() {
+        return coordArray;
+    }
+
+}
+
+/*
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ 
+package byui.cit260.solarTrails.model;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ *
+ * @author Joseph
+ 
 public class Character implements Serializable {
     private String name;
     private String description;
@@ -96,3 +147,5 @@ public class Character implements Serializable {
     
     
 }
+
+*/
